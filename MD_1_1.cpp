72ca80b0aba3c21749b2fdb64fe78bc9306898dd
Elvijs Kurss <you@example.com>
2025-10-24 15:42:08 +0300

    Test

--- MD_1_1.cpp
+++ MD_1_1.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {
 
+    cout << "Ieraksti skaitli, lai pāŗbaudītu vai tas ir palindroms" >> endl;git --version
+
+
+123
+
     system ("pause");
     return 0; 
 }
